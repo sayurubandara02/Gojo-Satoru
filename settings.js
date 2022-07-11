64beb02c8071d258b30447dcc8017effb302a75c
sayurubandara02 <109057182+sayurubandara02@users.noreply.github.com>
2022-07-11 10:16:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['94751312347']
+global.premium = ['94751312347']
+global.ownernomer = '94751312347'
+global.ownername = '🐦PANDA'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
